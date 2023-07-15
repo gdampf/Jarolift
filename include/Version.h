bd28d561ed07e8fd2282b5f5328c87d3421fdb28
gdampf <gdampf@gmx.de>
2023-07-15 23:31:02 +0200

    Switch from digitalRead to GPI / GP16I Register

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.1.4"
+        #define VERSION "2.2.1"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-07-10 06:18:59.758665"
+        #define BUILD_TIMESTAMP "2023-07-15 23:22:42.360521"
     #endif
     
