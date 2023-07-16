ffc0c04de52e4f18c40b85b2de13160fd92e65bd
gdampf <gdampf@gmx.de>
2023-07-16 11:59:03 +0200

    Versioning

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.2.2"
+        #define VERSION "2.2.3"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-07-16 11:41:15.248836"
+        #define BUILD_TIMESTAMP "2023-07-16 11:54:29.751344"
     #endif
     
